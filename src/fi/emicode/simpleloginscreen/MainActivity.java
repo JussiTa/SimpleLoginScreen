c3f42f4fc6ce4a3b7a9b361a8b9d4d0755dd3048
Mikko Ahonen <mikko@emicode.fi>
2012-10-03 14:07:01 +0300

    Complete version

--- src/fi/emicode/simpleloginscreen/MainActivity.java
+++ src/fi/emicode/simpleloginscreen/MainActivity.java
@@ -1,8 +1,11 @@
 package fi.emicode.simpleloginscreen;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 
@@ -17,4 +20,29 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public void onClick(View v){
+    	
+    	switch(v.getId()) {
+        case R.id.buttonOk:
+        
+        	Button ok = (Button) findViewById(R.id.buttonOk);
+        	ok.setVisibility(View.GONE);
+        	
+        	Button cancel = (Button) findViewById(R.id.buttonCancel);
+        	cancel.setVisibility(View.GONE);
+        	
+        	ProgressBar	progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        	progressBar.setVisibility(View.VISIBLE);
+        	 
+        break;
+        case R.id.buttonCancel:
+        
+        	this.finish();
+        	
+        break;
+    	}
+    	
+    }
+    
 }
